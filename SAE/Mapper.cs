04b32d74502730ffc23690832ef949ae05fd688e
Jürgen Huber <jue.huber@gmail.com>
2018-07-01 22:19:52 +0200

    enhanced settings

---
+++ SAE/Mapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE
+{
+    class Mapper
+    {
+        public static AIDifficulty TextToDiffictulty(string Difficulty)
+        {
+            switch (Difficulty)
+            {
+                case "Easy":
+                    return AIDifficulty.Easy;
+                case "Normal":
+                    return AIDifficulty.Normal;
+                default:
+                    return AIDifficulty.Normal;
+            }
+        }
+    }
+}
